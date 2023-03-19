aee473663770b6a3c6f59a996fcbf06612044f1b
Ilya Petrukhin <tvules.dev@gmail.com>
2023-03-19 19:59:54 +0400

    [Feature] Add start in webhook mode (#9)

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,19 +1,4 @@
-import asyncio
+from bot.main import run
 
-from aiogram import Bot, Dispatcher
-from aiogram.enums import ParseMode
-
-from bot.config import settings
-from bot.handlers import main_router
-
-
-async def main():
-    dp = Dispatcher()
-    dp.include_router(main_router)
-
-    await dp.start_polling(
-        Bot(settings.TOKEN, parse_mode=ParseMode.HTML),
-    )
-
-
-asyncio.run(main())
+if __name__ == "__main__":
+    run()
--- bot/config.py
+++ bot/config.py
@@ -1,9 +1,22 @@
-from pydantic import BaseSettings
+from typing import Optional
+
+from pydantic import BaseSettings, HttpUrl, SecretStr, validator
 
 
 class Settings(BaseSettings):
-    TOKEN: str
-    SECRET: str
+    token: SecretStr
+    secret: SecretStr
+
+    webhook_host: Optional[HttpUrl]
+    webhook_prefix: Optional[str] = "/telegram-webhook"
+    host: str = "127.0.0.1"
+    port: int = 8000
+
+    @validator("webhook_prefix")
+    def validate_webhook_prefix(cls, value: str):
+        if value and not value.startswith("/"):
+            raise ValueError("path should be started with `/` or be empty")
+        return value
 
     class Config:
         env_file = ".env"
