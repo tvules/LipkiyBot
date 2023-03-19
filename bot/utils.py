ebc9e2bd8d3903f7642df15f37dcabbbde9c447f
Ilya Petrukhin <tvules.dev@gmail.com>
2023-03-19 19:52:48 +0400

    Add a sticker creation from a message forwarded from a chat

--- bot/utils.py
+++ bot/utils.py
@@ -1,12 +1,14 @@
 from io import BytesIO
-from typing import Optional
+from typing import Optional, Union
 
-from aiogram import Bot, types
+import shortuuid
+from aiogram import Bot
+from aiogram.types import User
+
+from bot.config import settings
 
 
-async def download_user_avatar(
-    bot: Bot, user: types.User
-) -> Optional[BytesIO]:
+async def download_user_avatar(bot: Bot, user: User) -> Optional[BytesIO]:
     """Download a current user profile photo."""
 
     avatars = await bot.get_user_profile_photos(user_id=user.id, limit=1)
@@ -14,3 +16,9 @@ async def download_user_avatar(
         return None
 
     return await bot.download(avatars.photos[-1][-1], destination=BytesIO())
+
+
+def create_uuid_from_user_id(user_id: Union[int, str]) -> str:
+    """Create a short URL-safe uuid5 from user_id."""
+
+    return shortuuid.uuid(str(user_id) + settings.SECRET)
